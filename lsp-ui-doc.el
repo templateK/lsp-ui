6490a7e6e3067b2b15d6f1b1e956209d5fb41ff6
templateK <templateK@github.com>
2020-05-23 16:04:51 +0900

    Fix interference with the minibuffer of the parent frame.
    Don't reuse the minibuffer of the parent frame. Make new one.

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -641,8 +641,7 @@ HEIGHT is the documentation number of lines."
          (buffer (get-buffer name-buffer))
          (params (append lsp-ui-doc-frame-parameters
                          `((name . "")
-                           (default-minibuffer-frame . ,(selected-frame))
-                           (minibuffer . ,(minibuffer-window))
+                           (minibuffer . only)
                            (left-fringe . ,(frame-char-width))
                            (background-color . ,(face-background 'lsp-ui-doc-background nil t)))))
          (window (display-buffer-in-child-frame
